08b55f810f7ddbeccab9b06b99bdd160eb46ac94
Kartikey Rajput <141955327+kartikeyit@users.noreply.github.com>
2024-09-24 14:27:23 +0000

    variables in javascript

---
+++ 01_basics/variables.js
@@ -0,0 +1,26 @@
+const accountId = 24466
+let accountEmail = "kartikey@.com"
+let accountPassword = "password123"
+accountcity = "jaipur"
+
+
+// accountid = 2  not allowed 
+ 
+
+
+accountEmail = "hvcbn@224.com"
+accountPassword = "password1567"
+accountcity = "delhi"
+
+let accountstate;
+
+
+console.log(accountId);
+
+
+/*
+prefer not to used var because
+of issue in block scope and functional scope */
+
+
+console.table([accountId, accountEmail, accountPassword, accountcity]);
