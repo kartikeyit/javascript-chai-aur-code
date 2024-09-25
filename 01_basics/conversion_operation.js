8c76f0e74ae77184d8111397c6204357d3bebba9
Kartikey Rajput <141955327+kartikeyit@users.noreply.github.com>
2024-09-25 11:48:46 +0000

    coversion_operation

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,37 @@
+ 
+let score = "33"
+
+console.log(typeof score);  //string
+console.log(typeof(score));  //string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); //number 
+console.log(valueInNumber); //NaN
+
+
+
+
+
+
+
+ //"33" => 33
+ // "345abc" => NaN
+ // True => 1; false => 0
+ //"KARTIK" => NaN
+
+ let isloggedin = "vibhu"
+
+ let booleanisloggedin = Boolean(isloggedin)
+ console.log(booleanisloggedin);
+
+
+// 1=> true; 0 => false
+// "" => false
+// "vibhu" => true
+
+
+let somnumber = 33
+
+let stringnumber = string(somnumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
