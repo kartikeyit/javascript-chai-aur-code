2d8e0354d40b3a0b2eda12f8be40b698986ec9f0
Kartikey Rajput <141955327+kartikeyit@users.noreply.github.com>
2024-09-25 08:39:06 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,30 @@
+"use strict";
+ // treat all js code as newer version
+
+
+
+ let name = "vibhu"
+ let age = 18
+ let isloggedin = false
+ let state;
+   
+
+ // primitive data types
+
+ // number => 2 to power 53
+ // bigint
+ //string => ""
+ // boolean => true/false
+ // null =>  stabdalone value,  null is a repersentation of empty value
+ // undefined => means abhi assign hi nhi hui hai vo value 
+// symbol => symbol is used for find the uniqeness
+
+
+// object 
+
+console.log(typeof age);
+
+
+console.log(typeof undefined);  //undefined
+
+console.log(typeof null); //object
