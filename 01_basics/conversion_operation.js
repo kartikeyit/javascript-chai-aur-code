6bcb5d6e70d66a6a0ef0d22f59fe9d6747952727
Kartikey Rajput <141955327+kartikeyit@users.noreply.github.com>
2024-09-25 13:20:11 +0000

    operation

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -5,8 +5,8 @@ console.log(typeof score);  //string
 console.log(typeof(score));  //string
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber); //number 
-console.log(valueInNumber); //NaN
+// console.log(typeof valueInNumber); //number 
+// console.log(valueInNumber); //NaN
 
 
 
@@ -22,7 +22,7 @@ console.log(valueInNumber); //NaN
  let isloggedin = "vibhu"
 
  let booleanisloggedin = Boolean(isloggedin)
- console.log(booleanisloggedin);
+//  console.log(booleanisloggedin);
 
 
 // 1=> true; 0 => false
@@ -32,6 +32,48 @@ console.log(valueInNumber); //NaN
 
 let somnumber = 33
 
-let stringnumber = string(somnumber);
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// let stringnumber = string(somnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+
+//************************** operations ******************** */
+
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+ 
+
+
+
+let str1 = "hellow"
+let str2 = "vibhu"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); //12
+
+console.log(1 + "2");  //12
+console.log("1" + 2 +2); //122 
+// agr string first mai hai toh sabko string mai count kiya jayega
+console.log(1 + 2 + "2"); //32  yha string baad mai hai toh phle vala count ho jyega
+
+console.log(  (3+4) * 5 % 3);
+
+console.log(true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+// let gamecounter = 100;
+// gamecounter++;
+// console.log(gamecounter);
+
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+
+// read about prefix and postfix
