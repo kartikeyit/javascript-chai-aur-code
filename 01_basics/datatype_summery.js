5e42b4f1c47249071e5a7b1d19946539a41b4971
Kartikey Rajput <141955327+kartikeyit@users.noreply.github.com>
2024-09-27 11:59:04 +0000

    datatypesummery

---
+++ 01_basics/datatype_summery.js
@@ -0,0 +1,39 @@
+// primitive data types  
+// 7 types :  string , number, boolean, null, undefined
+            // symbol, bigint
+
+
+
+            const score = 100
+            const scorevalue = 100.3
+
+            const isloggedin = false
+            const outsidetemp = null
+            let useremail;
+
+             const id = symbol('123')
+             const anotherid = symbol('123')
+
+            console.log(id === anotherid);
+            const bignumber = 3444442553553535n
+    
+
+            // array, objects, functions
+
+            //array
+            const heros = ["shaktiman", "naagraj","doga"]  //array
+
+                let myobj = {
+                name: "kartik", //object
+                Age: 22,
+            }
+
+
+            const myfunction = function(){
+                console.log("hello world"); //function
+            }
+
+
+
+
+            console.log(typeof anotherid);
