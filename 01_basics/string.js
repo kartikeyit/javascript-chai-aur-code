5ad254ddf1270446a7c5d8c538e1b644425bf90d
Kartikey Rajput <141955327+kartikeyit@users.noreply.github.com>
2024-09-27 14:46:39 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,21 @@
+ const name = "kartikey"
+ const repocount = 50
+
+//  console.log(name + repocount)
+
+console.log(`hello my anme is ${name} and my repo count is $(repocount)`);
+
+
+const gamename =  new String('hitesh-hc')
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+
+console.log(gamename.indexOf('t'));
+
+
+const newstring = gamename.substring(0,4)
+console.log(newstring);
