1060e923039e8ea710a70b87b4d5be851d1745f6
Kartikey Rajput <141955327+kartikeyit@users.noreply.github.com>
2024-09-27 09:51:59 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,25 @@
+ //  basic conversion
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+
+
+// mostly cases mai inko use nhi krna hai avoid krna hai
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === cheack strictly   
+
+console.log("2" === 2);
